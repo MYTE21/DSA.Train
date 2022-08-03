700ca5b26afff72215416438f029da4ce6d611cf
Md. Yasmi Tohabar Evon (2016831021) <silverevon10010@gmail.com>
2022-08-03 22:18:15 +0600

    skeleton of doubly linked list

---
+++ Linked List/doubly_linked_list.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, data=None, prev=None, next=None):
+        self.data = data
+        self.prev = prev
+        self.next = next
+
+    def __repr__(self):
+        return repr(self.data)
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def append(self, data):
+        pass
+
+    def prepend(self, data):
+        pass
+
+    def search(self, item):
+        pass
+
+    def remove_node(self, node):
+        pass
+
+    def remove(self, item):
+        pass
